a2171304bf014d16c5c90d3792279ce8a7b2d62f
Ákos Szarvák <akos.szarvak@gmail.com>
2022-07-28 22:44:11 +0200

    added New Input System to character

---
+++ Assets/Scripts/Player/Input/PlayerInputHandler.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+
+  private Vector2 movementInput;
+    public void onMoveInput(InputAction.CallbackContext context)
+  {
+    movementInput = context.ReadValue<Vector2>();
+
+    if (context.performed)
+    {
+      Debug.Log(movementInput);
+    }
+  }
+
+  
+
+  public void onJumpInput(InputAction.CallbackContext context)
+  {
+    if(context.started){
+      Debug.Log("Jump pushed");
+    }
+    if (context.performed)
+    {
+      Debug.Log("Jump is held down");
+    }
+
+    if(context.canceled){
+      Debug.Log("Jump is released");
+    }
+  }
+}
